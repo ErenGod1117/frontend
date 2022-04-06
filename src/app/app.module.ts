bae136bdf02a469a62ea4b7b44f106357b5edc1c
ErenGod1117 <onepiecesagawano@gmail.com>
2022-04-06 00:35:54 -0500

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { LoginComponent } from './home/login/login.component';
+import { SingUpComponent } from './home/sing-up/sing-up.component';
+import { ListTaskComponent } from './task/list-task/list-task.component';
+import { CreateTaskComponent } from './task/create-task/create-task.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { FooterComponent } from './home/footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SingUpComponent,
+    ListTaskComponent,
+    CreateTaskComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
